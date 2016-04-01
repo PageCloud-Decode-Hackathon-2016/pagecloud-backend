0563716ea367571f063f4e29a8d954124981ad59
Bheesham Persaud <bheesham.persaud@live.ca>
2016-04-01 18:45:58 -0400

    ReferrerImplement referrers in the format that was specified.

--- api.py
+++ api.py
@@ -1,8 +1,10 @@
+from collections import defaultdict
 from flask import Flask
 from flask_restful import Resource, Api
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search, Q
 import json
+from urlparse import urlparse
 
 app = Flask(__name__)
 api = Api(app)
@@ -14,11 +16,8 @@ client = Elasticsearch(host='search-pagecloud-legacy-decode-2016-oijvlfnyaac4p6h
 
 class Referrers(Resource):
     def get(self):
+        counts = defaultdict(int)
         results = []
-
-        # s = Search(using=client, index='production-logs-*')\
-        #     .fields(['agent', 'clientip', 'referrer', 'timestamp'])\
-        #     .query('match_all')
 
         s = Search(using=client, index='production-logs-*')\
             .fields(['referrer'])\
@@ -27,9 +26,18 @@ class Referrers(Resource):
         response = s.execute().to_dict()
 
         for hit in response['hits']['hits']:
-        	results.append(hit['fields']['referrer'][0].replace('"', ''))
+            url = urlparse(hit['fields']['referrer'][0].replace('"', '')).netloc
+            counts[url] = counts.get(url, 0) + 1
 
-        return results
+        for site in counts.keys():
+            results.append({
+                'name': site,
+                'count': counts[site]
+            })
+
+        return {
+            'data': results
+        }
 
 class Geo(Resource):
     def get(self):
